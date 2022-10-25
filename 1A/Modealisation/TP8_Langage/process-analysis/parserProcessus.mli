d351c0ef1953c22f4c816b90d6c2b8bbd552d759
Canon Ayoub <acanon@carapuce.enseeiht.fr>
2022-10-25 18:15:10 +0200

    Modelisation Language TP8

---
+++ 1A/Modealisation/TP8_Langage/process-analysis/parserProcessus.mli
@@ -0,0 +1,27 @@
+
+(* The type of tokens. *)
+
+type token = 
+  | STARTS
+  | STARTED
+  | RIGHT_BRACE
+  | RESOURCE
+  | REQUIRES
+  | PROCESS
+  | NUMBER of (int)
+  | LEFT_BRACE
+  | IF
+  | IDENTIFIER of (string)
+  | FINISHES
+  | FINISHED
+  | END
+  | AMOUNT
+  | ACTIVITY
+
+(* This exception is raised by the monolithic API functions. *)
+
+exception Error
+
+(* The monolithic API. *)
+
+val fichier: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (unit)
