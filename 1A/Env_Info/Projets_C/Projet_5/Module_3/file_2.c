edb16b5c2cee22142c7037d040bc605535662c67
Canon Ayoub <acanon@carmack.enseeiht.fr>
2022-10-25 08:48:51 +0200

    TP Env_Info

--- 1A/Env_Info/Projets_C/Projet_5/Module_3/file_2.c
+++ 1A/Env_Info/Projets_C/Projet_5/Module_3/file_2.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 int main() {
-   printf("Je suis file_2.c dans Module_3 dans Projet_5");
+   afficher("Je suis file_2.c dans Module_3 dans Projet_5");
 return;
 }
