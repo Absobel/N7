7021d1afa7ced69979aa10041ad296b9dfd405fa
Absobel <acanon@banshee.enseeiht.fr>
2022-11-14 18:01:31 +0100

    Opti TP diff finies

---
+++ 1A/Modealisation/BE_2019/ex2.v
@@ -0,0 +1,17 @@
+Require Import Naturelle.
+Section Session1_2019_Logique_Exercice_2.
+
+Variable A B : Prop.
+
+Theorem Exercice_2_Naturelle : (~A) \/ B -> (~A) \/ (A /\ B).
+Proof.
+(* A COMPLETER *)
+Qed.
+
+Theorem Exercice_2_Coq : (~A) \/ B -> (~A) \/ (A /\ B).
+Proof.
+(* A COMPLETER *)
+Qed.
+
+End Session1_2019_Logique_Exercice_2.
+
