5c24a2c014aa9215182475e1a60ccc3546169fb1
Absobel <imkonnu@gmail.com>
2023-01-25 18:02:36 +0100

    Java project bien avancé

--- 1A/Traitement du signal/Projet/demoduler.m
+++ 1A/Traitement du signal/Projet/demoduler.m
@@ -1,16 +1,4 @@
 function sortie = demoduler(signal)
-    F0 = 1180; F1 = 980;
-    theta0 = rand*2*pi; theta1 = rand*2*pi;
-    Fe = 48000;
-    Te = 1/Fe;
-    Rb = 300;
-    Ts = 1/Rb;
-    Ns = floor(Ts/Te);
-    Nbits = length(signal)/Ns;
-    bits = randi([0 1],1,Nbits);
-    NRZ = kron(bits,ones(1,Ns));
-    long = length(NRZ);
-    t = 0:Te:(long-1)*Te;
 
     x_int0_0 = reshape(signal.*cos(2*pi*F0*t+theta0),Ns,Nbits);
     x_int00_0 = trapz(x_int0_0);
@@ -20,7 +8,7 @@ function sortie = demoduler(signal)
     x_int00_2 = trapz(x_int0_1);
     x_int00_3 = x_int00_2.^2;
 
-    x_int1_0 = reshape(signal.*cos(2*pi*F1*t+theta1),Ns,Nbits);
+    x_int1_0 = reshape(signal.*cos(2*pi*F1*t+theta1),Ns,Nbits); 
     x_int11_0 = trapz(x_int1_0);
     x_int11_1 = x_int11_0.^2;
 
