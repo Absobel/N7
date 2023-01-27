ab63e8dd3fcf8af4e2a8d4d11f30dbe1d3c9cfb4
Absobel <imkonnu@gmail.com>
2023-01-27 06:29:10 +0100

    projet tds fini ?

--- 1A/Traitement du signal/Projet/images.m
+++ 1A/Traitement du signal/Projet/images.m
@@ -1,17 +1,26 @@
-% 6.2.2.c
+% PARAMÈTRES
+
+Fe = 48000;          % Fréquence d'échantillonage
+Rb = 300;            % Débit binaire
+Nbits = 1000;        % Nombre de bits à transmettre
+F0 = 1180;           % Fréquence des bits à 0 
+F1 = 980;            % Fréquence des bits à 0 
+
+theta0 = rand*2*pi; theta1 = rand*2*pi;
+
 
 load 'FICHIERS POUR ELEVES IMAGES/fichier1.mat';
-img1 = demoduler(signal);
+img1 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 load 'FICHIERS POUR ELEVES IMAGES/fichier2.mat';
-img2 = demoduler(signal);
+img2 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 load 'FICHIERS POUR ELEVES IMAGES/fichier3.mat';
-img3 = demoduler(signal);
+img3 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 load 'FICHIERS POUR ELEVES IMAGES/fichier4.mat';
-img4 = demoduler(signal);
+img4 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 load 'FICHIERS POUR ELEVES IMAGES/fichier5.mat';
-img5 = demoduler(signal);
+img5 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 load 'FICHIERS POUR ELEVES IMAGES/fichier6.mat';
-img6 = demoduler(signal);
+img6 = demoduler(signal,Fe,Rb,F0,F1,theta0,theta1);
 
 nimg1 = reconstitution_image(img1);
 nimg2 = reconstitution_image(img2);
