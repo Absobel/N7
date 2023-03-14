03e7c8899ce52bf0647371dcd2b1b859c8dfe033
Absobel <imkonnu@gmail.com>
2023-03-14 14:11:06 +0100

    projet cs + debut tp 1

---
+++ 1A/CalculScientifique/TP1/mgs.m
@@ -0,0 +1,20 @@
+%--------------------------------------------------------------------------
+% ENSEEIHT - 1SN - Calcul scientifique
+% TP1 - Orthogonalisation de Gram-Schmidt
+% mgs.m
+%--------------------------------------------------------------------------
+
+function Q = mgs(A)
+
+    % Recuperation du nombre de colonnes de A
+    [~, m] = size(A);
+    
+    % Initialisation de la matrice Q avec la matrice A
+    Q = A;
+    
+    %------------------------------------------------
+    % A remplir
+    % Algorithme de Gram-Schmidt modifie
+    %------------------------------------------------
+
+end
