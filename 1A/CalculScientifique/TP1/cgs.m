388deedf54477efc636751183b15a266ef040b94
Absobel <imkonnu@gmail.com>
2023-03-14 15:46:14 +0100

    tp1 cs

--- 1A/CalculScientifique/TP1/cgs.m
+++ 1A/CalculScientifique/TP1/cgs.m
@@ -16,5 +16,15 @@ function Q = cgs(A)
     % A remplir
     % Algorithme de Gram-Schmidt classique
     %------------------------------------------------
-
+    
+    Q(:,1) = A(:,1)/norm(A(:,1));
+    for j = 2:m
+        sum = 0;
+        for i = 1:j-1
+            sum = sum + dot(Q(:,i),A(:,j))*Q(:,i);  % dot()
+            %sum = sum + (Q(:,i)'*A(:,j))*Q(:,i);  % produit scalaire
+        end
+        Q(:,j) = A(:,j) - sum;
+        Q(:,j) = Q(:,j)/norm(Q(:,j));
+    end
 end
