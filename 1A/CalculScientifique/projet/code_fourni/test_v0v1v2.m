4f517bc9f5ee76bd2c633d343d254b4b837acf67
Absobel <imkonnu@gmail.com>
2023-04-18 09:19:20 +0200

    juste avant de faire subspace_iter_v3

--- 1A/CalculScientifique/projet/code_fourni/test_v0v1v2.m
+++ 1A/CalculScientifique/projet/code_fourni/test_v0v1v2.m
@@ -1,12 +1,15 @@
 clear all;
 format long;
 
+for n = 100:100:300
+    for imat = 1:4
+
 %%%%%%%%%%%%
 % PARAMÈTRES
 %%%%%%%%%%%%
 
 % taille de la matrice symétrique
-n = 200;
+%n = 200;
 
 % type de la matrice (voir matgen_csad)
 % imat == 1 valeurs propres D(i) = i
@@ -14,15 +17,19 @@ n = 200;
 %                                  uniformément répartie, cond = 1e10
 % imat == 3 valeurs propres D(i) = cond**(-(i-1)/(n-1)) avec cond = 1e5
 % imat == 4 valeurs propres D(i) = 1 - ((i-1)/(n-1))*(1 - 1/cond) avec cond = 1e2
-imat = 1;
+%imat = 1;
 
 % tolérance
 eps = 1e-8;
 % nombre d'itérations max pour atteindre la convergence
 maxit = 10000;
 
+
+
+
+
 % on génère la matrice (1) ou on lit dans un fichier (0)
-genere = 1;
+genere = 0;
 
 % méthode de calcul
 v = 0; % subspace iteration v0
@@ -34,6 +41,13 @@ m = 20;
 
 fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
 fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+
+
+
+
+
+% on génère la matrice (1) ou on lit dans un fichier (0)
+genere = 0;
 
 % méthode de calcul
 v = 1; % subspace iteration v1
@@ -49,3 +63,31 @@ genere = 0;
 
 fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
 fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+
+
+
+
+
+
+for p = 1:4
+% on génère la matrice (1) ou on lit dans un fichier (0)
+genere = 0;
+
+% méthode de calcul
+v = 2; % subspace iteration v2
+
+% taille du sous-espace (V1, v2, v3)
+m = 20;
+
+% pourcentage de la trace que l'on veut atteindre (v1, v2, v3)
+percentage = .1;
+
+genere = 0;
+[W, V, flag, q, qv] = eigen_2023(imat, n, v, m, eps, maxit, percentage, p, genere);
+
+fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
+fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+end
+
+    end
+end
--- 1A/CalculScientifique/projet/code_fourni/test_v11v12.m
+++ 1A/CalculScientifique/projet/code_fourni/test_v11v12.m
@@ -1,12 +1,14 @@
 clear all;
 format long;
 
+for n = 100:100:300
+    for imat = 1:4
 %%%%%%%%%%%%
 % PARAMÈTRES
 %%%%%%%%%%%%
 
 % taille de la matrice symétrique
-n = 200;
+% n = 100;     % overwritten by for loop
 
 % type de la matrice (voir matgen_csad)
 % imat == 1 valeurs propres D(i) = i
@@ -14,7 +16,7 @@ n = 200;
 %                                  uniformément répartie, cond = 1e10
 % imat == 3 valeurs propres D(i) = cond**(-(i-1)/(n-1)) avec cond = 1e5
 % imat == 4 valeurs propres D(i) = 1 - ((i-1)/(n-1))*(1 - 1/cond) avec cond = 1e2
-imat = 4;
+%imat = 4;  % overwritten by for loop
 
 % on génère la matrice (1) ou on lit dans un fichier (0)
 % si vous avez déjà généré la matrice d'une certaine taille et d'un type donné
@@ -25,9 +27,6 @@ genere = 1;
 % nombre maximum de couples propres calculés
 m = 50;
 percentage = 0.4;
-
-% on génère la matrice (1) ou on lit dans un fichier (0)
-genere = 0;
 
 % méthode de calcul
 v = 11; % basic power method
@@ -62,3 +61,6 @@ maxit = 10000;
 
 fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
 fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+
+    end
+end
