1cdc6b07cd4264d8491ae34094f9ad57984448a4
Absobel <imkonnu@gmail.com>
2023-11-17 12:49:23 +0100

    metaprog + pf2

--- 2A/GLS/Metaprog/TP2-JUnit-Introspection/src/LanceurIndependant.java
+++ 2A/GLS/Metaprog/TP2-JUnit-Introspection/src/LanceurIndependant.java
@@ -1,10 +1,11 @@
 import java.lang.reflect.*;
 import java.util.*;
 
-/** L'objectif est de faire un lanceur simple sans utiliser toutes les clases
-  * de notre architecture JUnit.   Il permet juste de valider la compréhension
-  * de l'introspection en Java.
-  */
+/**
+ * L'objectif est de faire un lanceur simple sans utiliser toutes les clases
+ * de notre architecture JUnit. Il permet juste de valider la compréhension
+ * de l'introspection en Java.
+ */
 public class LanceurIndependant {
 	private int nbTestsLances = 0;
 	private int nbErreurs = 0;
@@ -12,7 +13,7 @@ public class LanceurIndependant {
 	private List<Throwable> erreurs = new ArrayList<>();
 
 	public LanceurIndependant(String... nomsClasses) {
-	    System.out.println();
+		System.out.println();
 
 		// Lancer les tests pour chaque classe
 		for (String nom : nomsClasses) {
@@ -40,26 +41,21 @@ public class LanceurIndependant {
 				nbTestsLances, nbEchecs, nbErreurs);
 	}
 
-
 	public int getNbTests() {
 		return this.nbTestsLances;
 	}
-
 
 	public int getNbErreurs() {
 		return this.nbErreurs;
 	}
 
-
 	public int getNbEchecs() {
 		return this.nbEchecs;
 	}
 
-
 	private void testerUneClasse(String nomClasse)
-		throws ClassNotFoundException, InstantiationException,
-						  IllegalAccessException
-	{
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException {
 		// Récupérer la classe
 		Class<?> classe = Class.forName(nomClasse);
 
@@ -70,7 +66,7 @@ public class LanceurIndependant {
 		} catch (NoSuchMethodException e) {
 			preparer = null;
 		}
-	
+
 		Method nettoyer;
 		try {
 			nettoyer = classe.getMethod("nettoyer");
@@ -79,9 +75,59 @@ public class LanceurIndependant {
 		}
 
 		// Instancier l'objet qui sera le récepteur des tests
-		Object objet = null;
+		Object objet;
+		try {
+			objet = classe.getConstructor().newInstance();
+		} catch (Exception e) {
+			System.out.println("Erreur lors de l'instanciation de la classe :");
+			e.printStackTrace();
+			return;
+		}
 
 		// Exécuter les méthods de test
+		if (preparer != null) {
+			try {
+				preparer.invoke(objet);
+			} catch (InvocationTargetException e) {
+				System.out.println("Erreur dans la méthode preparer :");
+				e.getCause().printStackTrace();
+				return;
+			}
+		}
+
+		for (Method methode : classe.getMethods()) {
+			if (methode.getName().startsWith("test") && !Modifier.isStatic(methode.getModifiers())) {
+				try {
+					methode.invoke(objet);
+				} catch (InvocationTargetException e) {
+					Throwable cause = e.getCause();
+					if (cause instanceof Echec) {
+						System.out.println("Echec du test " + methode.getName() + " : ");
+						System.out.println(cause.getMessage());
+						nbEchecs++;
+					} else {
+						System.out.println("Erreur dans le test " + methode.getName() + " : ");
+						nbErreurs++;
+						erreurs.add(cause);
+					}
+				} catch (Exception e) {
+					System.out.println("Erreur dans le test " + methode.getName() + " : ");
+					nbErreurs++;
+					erreurs.add(e);
+				}
+				nbTestsLances++;
+			}
+		}
+
+		if (nettoyer != null) {
+			try {
+				nettoyer.invoke(objet);
+			} catch (InvocationTargetException e) {
+				System.out.println("Erreur dans la méthode nettoyer :");
+				e.getCause().printStackTrace();
+				return;
+			}
+		}
 	}
 
 	public static void main(String... args) {
