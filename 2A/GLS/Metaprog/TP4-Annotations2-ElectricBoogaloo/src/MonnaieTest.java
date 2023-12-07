b99ab18db9944ed575223af1312f18c451b940e1
Absobel <imkonnu@gmail.com>
2023-12-07 08:24:20 +0100

    dhf

---
+++ 2A/GLS/Metaprog/TP4-Annotations2-ElectricBoogaloo/src/MonnaieTest.java
@@ -0,0 +1,28 @@
+import annotations.Avant;
+import annotations.UnTest;
+
+/** Classe regroupant les tests unitaires de la classe Monnaie.  */
+public class MonnaieTest {
+
+	protected Monnaie m1;
+	protected Monnaie m2;
+
+	@Avant
+	public void preparer() {
+		this.m1 = new Monnaie(5, "euro");
+		this.m2 = new Monnaie(7, "euro");
+	}
+
+	@UnTest
+	public void testerAjouter() throws DeviseInvalideException {
+		m1.ajouter(m2);
+		Assert.assertTrue(m1.getValeur() == 12);
+	}
+
+	@UnTest
+	public void testerRetrancher() throws DeviseInvalideException {
+		m1.retrancher(m2);
+		Assert.assertTrue(m1.getValeur() == -2);
+	}
+
+}
---
+++ 2A/GLS/Metaprog/TP4-Annotations2-ElectricBoogaloo/src/MonnaieTest2.java
@@ -0,0 +1,15 @@
+import annotations.UnTest;
+
+/** MonnaieTest2 : Vérifier que les méthodes de test de la superclasse sont bien
+  * prises en compte !
+  * @author	Xavier Crégut
+  * @version	$Revision$
+  */
+public class MonnaieTest2 extends MonnaieTest {
+
+  @UnTest
+	public void testSupplementaire() {
+		// OK.
+	}
+
+}
